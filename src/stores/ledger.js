391727fdd766e1eda7d55b6aeff3d71f27af94b9
bonrki9 <bonkri999@gmail.com>
2023-12-28 22:36:58 +0900

    [feat] stores

--- src/stores/ledger.js
+++ src/stores/ledger.js
@@ -7,8 +7,104 @@ const REST_LEDGER_API = `http://localhost:8080/api/ledger`
 
 export const useLedgerStore = defineStore('ledger', () => {
 
+  //등록
+  const registLedger = function(Ledger){
+    axios({
+      url : REST_LEDGER_API,
+      method : "POST",
+      params : Ledger
+    })
+    .then((res)=>{
+      console.log(res.data)
+    })
+    .catch((err)=>console.log(err))
+  }
 
+  //전체 조회
+  const ledgerList = ref([]);
+  const selectLedgerAll = function(){
+    axios({
+      url: `${REST_LEDGER_API}/`,
+      method: "GET",
+    }).then((res) => {
+      ledgerList.value = res.data;
+      console.log(ledgerList.value);
+    })
+  }
+
+  //하나 조회
+  const ledgerOne = ref({});
+  const selectLedgerOne = function(LedgerId) {
+    axios({
+      url: `${REST_LEDGER_API}/${LedgerId}`,
+      method: "GET",
+    }).then((res) => {
+      ledgerOne.value = res.data;
+    });
+  };
+
+  //수정
+  const updateLedger = function(Ledger){
+    axios.put(REST_LEDGER_API, Ledger)
+    .then(()=>router.push(`/legder`))
+    .catch((err)=>console.log(err))
+  }
+
+  //삭제
+  const deleteLedger = function(LedgerId){
+    axios({
+      url : REST_LEDGER_API,
+      method : "DELETE",
+      params : {LedgerId : LedgerId},
+    })
+    .then(()=>router.push("/ledger"))
+    .catch((err)=>console.log(err))
+  }
+
+  //현재 달 총 수입
+  const selectLedgerIncome = function(category){
+    axios({
+      url : REST_LEDGER_API,
+      method : "GET",
+      params : {category : category},
+    })
+    .then(()=>router.push("/ledger"))
+    .catch((err)=>console.log(err))
+  }
+
+  //현재 달 총 지출
+  const selectLedgerExpenditure = function(category){
+    axios({
+      url : REST_LEDGER_API,
+      method : "GET",
+      params : {category : category},
+    })
+    .then(()=>router.push("/ledger"))
+    .catch((err)=>console.log(err))
+  }
+
+  //월별 가계부
+  const selectLedgerByMonth = function(budgetDate){
+    axios({
+      url : REST_LEDGER_API,
+      method : "GET",
+      params : {budgetDate : budgetDate},
+    })
+    .then(()=>router.push("/ledger"))
+    .catch((err)=>console.log(err))
+  }
+
+  //일별 가계부
+  const selectLedgerByDay = function(budgetDate){
+    axios({
+      url : REST_LEDGER_API,
+      method : "GET",
+      params : {budgetDate : budgetDate},
+    })
+    .then(()=>router.push("/ledger"))
+    .catch((err)=>console.log(err))
+  }
   
-  return{  }
+  return{ ledgerList, ledgerOne, registLedger, selectLedgerAll, selectLedgerOne, updateLedger, deleteLedger, selectLedgerIncome, selectLedgerExpenditure }
 
 },{persist : true})
--- src/views/LedgerView.vue
+++ src/views/LedgerView.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="ledger-container">
     가계부 뷰
-
+    <canvas id="myChart" width="400" height="400"></canvas>
     <RouterView/>
   </div>
 </template>
 
-<script setup>
+<script >
 import { RouterView } from 'vue-router';
+import Chart from 'chart.js/auto';
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+  
+  const config = {
+    type: 'line',
+    data: data,
+    options: {},
+  };
+  
+  onMounted(() => {
+      
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+  }),
 </script>
 
 <style scoped>
