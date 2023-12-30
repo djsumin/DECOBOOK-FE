f94d74e248a2eb489c6e79032d5e8834dcecebcd
bonrki9 <bonkri999@gmail.com>
2023-12-30 14:27:01 +0900

    [feat] chart.js

--- src/components/ledger/LedgerMonthlyList.vue
+++ src/components/ledger/LedgerMonthlyList.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>가계부 달력 뷰</div>
+  <div>
+    <RouterLink :to="{name: 'ledgerList'}">리스트 보기</RouterLink>
+  </div>
+  <RouterView/>
+  <div class ="diary-container">
+    <h2>가계부 달력 뷰</h2>
+    <FullCalendar class="calendar" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import FullCalendar from '@/components/ledger/LedgerFullcalendar.vue'
+import { RouterView } from 'vue-router';
 
-<style scoped></style>
+
+</script>
+
+<style scoped>
+.calendar {
+  margin-top: 30px;
+  font-size: 14px;
+  width: 700px;
+}
+
+.diary-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
--- src/views/LedgerView.vue
+++ src/views/LedgerView.vue
@@ -4,25 +4,80 @@
     <div class="charts-container">
       <div class="chart-item">
         <h2>달력</h2>
+        <RouterLink :to="{ name: 'ledgerMonthlyList' }">달력 보기</RouterLink>
       </div>
       <div class="chart-item">
         <h2>막대그래프</h2>
         <button @click="goList()">자세히보기</button>
+        <canvas ref="MyChart"></canvas>
       </div>
       <div class="chart-item">
         <h2>원형그래프</h2>
         <button @click="goCategory()">자세히보기</button>
       </div>
     </div>
-    <button @click="createLedger()">오늘의 수입/지출 남기기</button>
-    <canvas id="myChart" width="400" height="400"></canvas>
+    <button @click="createLedger()">수입/지출 등록하기</button>
+    <!-- <canvas id="myChart" width="400" height="400"></canvas> -->
     <RouterView />
   </div>
 </template>
 
-<script setup>
+<script >
 import router from "@/router";
 import { RouterView } from "vue-router";
+import { Chart, registerables} from 'chart.js';
+Chart.register(...registerables);
+
+export default {
+  data:() => ({
+    type: 'bar',
+    data: {
+      labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
+      datasets: [{
+        label: '# of Votes',
+        data: [ 12, 19, 3, 5, 2, 3 ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  }),
+  mounted(){
+    this.createChart()
+  },
+  methods:{
+    createChart(){
+      new Chart(this.$refs.MyChart, {
+        type:'bar',
+        data:this.data,
+        options:this.options
+      })
+
+    }
+  }
+
+}
 
 const goList = function () {
   router.push({ name: "ledgerList" });
